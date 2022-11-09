6faf61de07e9b84e74979b2a354df863b9f80ec1
naveenSaini1 <naveensaini94667@gmail.com>
2022-11-09 14:06:26 +0530

    init

---
+++ Tripy/src/main/java/com/tripy/TripyApplication.java
@@ -0,0 +1,13 @@
+package com.tripy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripyApplication.class, args);
+	}
+
+}
---
+++ Tripy/src/test/java/com/tripy/TripyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
