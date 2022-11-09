696b619a3e9d6827e126bc610da580b89a3a87f5
raje2 <100850727+raje2@users.noreply.github.com>
2022-11-10 01:29:45 +0530

    upadate

---
+++ Tripy/src/main/java/com/tripy/customerexception/CustomerException.java
@@ -0,0 +1,17 @@
+package com.tripy.customerexception;
+
+public class CustomerException extends Exception{
+
+	public CustomerException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
