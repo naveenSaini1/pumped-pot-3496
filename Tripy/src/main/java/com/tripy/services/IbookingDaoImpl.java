db468736336eb7fb61c6bd4f5714d9fb2b0d68ea
vijayhandal <handalvijay9588@gmail.com>
2022-11-10 12:36:19 +0530

    update DAY-3

---
+++ Tripy/src/main/java/com/tripy/services/IbookingDaoImpl.java
@@ -0,0 +1,42 @@
+package com.tripy.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tripy.models.Booking;
+import com.tripy.repositary.IBookingRepository;
+
+@Service
+public class IbookingDaoImpl implements IBookingService{
+	
+	@Autowired
+	private IBookingRepository IbookRepo;
+
+	@Override
+	public Booking makeBooking(Booking booking) {
+		// TODO Auto-generated method stub
+		Booking b = IbookRepo.save(booking);
+		return b;
+	}
+
+	@Override
+	public Booking cancelBooking(Integer bookingId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Booking viewBooking(Integer bookingId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Booking> viewAllBookings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
